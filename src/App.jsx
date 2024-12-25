bda5a99e78ffe2cf8599f622c25cf6648e2dd873
ashish <ashishkumar.b3011@gmail.com>
2024-12-25 21:14:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Convert from "./Components/Convert"
+
+function App() {
+  return (
+    <div className="currency-converter">
+      <h1 className="converter-title">Currency converter</h1>
+      < Convert />
+
+    </div>
+  )
+}
+
+export default App
